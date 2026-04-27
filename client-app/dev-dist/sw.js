09e423cd7ccfe4493df20ff612050d2ac4880dae
Pranav-trackway <pranav@ohmgpt.com>
2026-04-27 14:21:54 +0530

    fixed dependecny issue in client-app for pwa

--- client-app/dev-dist/sw.js
+++ client-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0kmo3vi9ek"
+    "revision": "0.rmbqsaeoojk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
